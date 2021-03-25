7d9d920d4bfd55cbc24e47a84e8ff99f0ed4798f
Israel <israel.schmitt.j@gmail.com>
2021-03-24 23:02:44 -0300

    desafios

---
+++ prova1/chocolate_2.py
@@ -0,0 +1,7 @@
+import math
+
+lado = int(input())
+
+n = int(math.log(lado, 2))
+
+print(4**n)
--- prova1/detectando_colisoes_2.py
+++ prova1/detectando_colisoes_2.py
@@ -3,11 +3,7 @@ r1 = {"x0": x0, "y0": y0, "x1": x1, "y1": y1}
 x0, y0, x1, y1 = [int(w) for w in input().split()]
 r2 = {"x0": x0, "y0": y0, "x1": x1, "y1": y1}
 
-if (r1["x0"] <= r2["x1"] and
-    r1["x1"] >= r2["x0"] and
-    r1["y0"] <= r2["y1"] and
-    r1["y1"] >= r2["y0"]
-    ):
-    print(1)
-else:
-    print(0) 
+print(int(r1["x0"] <= r2["x1"] and
+          r1["x1"] >= r2["x0"] and
+          r1["y0"] <= r2["y1"] and
+          r1["y1"] >= r2["y0"]))
